99f40fd1963bcb55061c3e5b61b8aae0635ec73a
pauladev11 <teacher.strumienieschool@gmail.com>
2025-09-26 15:07:14 +0800

    staticfiles dirs

--- farmsite/settings.py
+++ farmsite/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
 
 if DEBUG:
     STATICFILES_DIRS = [
